38b8e0a83b9708a9bf5b97e59a0de5fefc7f6e3b
sgradisar <sebastian.gradisar@gmail.com>
2023-11-08 23:17:54 +0100

    Api test and fetching data
    - created an env file with public/private key;
    - configured the data to test responses;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import md5 from "crypto-js/md5";
+
+async function fetchComics() {
+	const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+	const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+	const timestamp = Number(new Date());
+
+	const hash = md5(timestamp + privateKey + publicKey).toString();
+
+	try {
+		const response = await fetch(
+			`https://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`
+		);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+
+		return data;
+	} catch (error) {
+		console.error("Fetch error: ", error);
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	fetchComics().then((data) => console.log(data));
+
+	return <div className="App"></div>;
 }
 
 export default App;
